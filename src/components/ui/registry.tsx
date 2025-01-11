c45441da4a1a96649249ba8467c0de3c2868f70d
vavgithub <helloatvavgithub.com>
2025-01-12 03:03:08 +0530

    Folder Structure Created

---
+++ src/components/ui/registry.tsx
@@ -0,0 +1,10 @@
+// src/components/ui/registry.tsx
+import { Switch } from "./switch"
+import { Slider } from "./slider"
+import { Tooltip } from "./tooltip"
+
+export const components = {
+  Switch,
+  Slider,
+  Tooltip,
+}
